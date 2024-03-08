66ad459205f707087cbc4c00548e1420f174798c
omwomo <eugeneoduor79@gmail.com>
2024-03-08 21:39:02 +0300

    update project description

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ const projects = [
   },
   {
     name: 'series review portal',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: 'This is an interactive platform allowing users to explore, rate, and engage with their favorite TV shows. The "Series Review Portal" empowers users to comment, like, and delve into comprehensive details for an enriched entertainment experience.',
     description2: 'The app utilizes reliable weather APIs to provide up-to-date data, ensuring users are equipped with the most accurate information. Whether you\'re planning a weekend getaway or simply want to know if you\'ll need an umbrella, this app has you covered.',
     featuredImage: 'img/series-review.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
